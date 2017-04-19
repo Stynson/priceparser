569cc573fc4618f6cde61958c03f5a6d1b2c91f8
Bálint KISS <balint.kiss@nng.com>
2017-04-19 19:09:38 +0200

    save progress

---
+++ proj/proj/spiders/config.py
@@ -0,0 +1,7 @@
+
+
+urlMap = {
+   "https://notebook-memoria.arukereso.hu/kingston/hyperx-impact-16gb-2x8gb-ddr4-2133mhz-hx421s13ibk2-16-p309241599/" : "Kingstone 16GB DDR4 2133Mhz SO-DIMM"
+    ,"https://www.arukereso.hu/processzor-c3139/intel/core-i5-6500-3-2ghz-lga1151-p303168139/" : "Intel Core i5-6500"  
+    ,"https://www.arukereso.hu/szamitogep-haz-c3085/silverstone/vital-sst-vt01-p356074737/" : "SilverStone Vital VT01"
+}
--- proj/proj/spiders/priceparser.py
+++ proj/proj/spiders/priceparser.py
@@ -1,55 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import time
+from config import urlMap 
 
 class PriceParserSpider(scrapy.Spider):
     name = "priceparser"
-    #allowed_domains = ["example.com"]
-    start_urls = ['https://www.arukereso.hu/szamitogep-haz-c3085/zalman/m1-p230370475/']
+    allowed_domains = ["arukereso.hu"]
+    start_urls = urlMap.keys()
+    with open("result.jl","a") as f: 
+        f.write('{ "crawl-time" : ' + str(time.time()) + " }\n") #new crawl marker
 
     def parseOne(self,string):
-        attribList = string.split('onclick')[1]
+        attribList = string.split('onclick')[1].split('target')[0]
         attribs = attribList.split(',')
         return '{ "' + "-".join(attribs[-9].split("-")[:-1]).strip().strip("'") + '" : "' + attribs[-2].strip().strip("'") + '" }'
 
     def parse(self, response):
-        #xpath = response.xpath('//*[@id="offer-list"]/div[1]/div[1]/div/div[5]/a')
         xpath = response.xpath('//a[@class="price"]')
-        with open("result.jl","w") as f: 
+        with open("result.jl","a") as f: 
+            f.write('{ "name" : "' + urlMap[response.url] + '" }\n')
             res = xpath.extract()
             for r in res: 
-                f.write(self.parseOne(r) + "\n")
-
-
-
-
-#from scrapy import signals
-#from scrapy.exporters import XmlItemExporter
-
-#class XmlExportPipeline(object):
-
-     #def __init__(self):
-        #self.files = {}
-
-     #@classmethod
-     #def from_crawler(cls, crawler):
-         #pipeline = cls()
-         #crawler.signals.connect(pipeline.spider_opened, signals.spider_opened)
-         #crawler.signals.connect(pipeline.spider_closed, signals.spider_closed)
-         #return pipeline
-
-    #def spider_opened(self, spider):
-        #file = open('%s_products.xml' % spider.name, 'w+b')
-        #self.files[spider] = file
-        #self.exporter = XmlItemExporter(file)
-        #self.exporter.start_exporting()
-
-    #def spider_closed(self, spider):
-        #self.exporter.finish_exporting()
-        #file = self.files.pop(spider)
-        #file.close()
-
-    #def process_item(self, item, spider):
-        #self.exporter.export_item(item)
-        #return item
-
+                f.write(self.parseOne(r.encode('utf-8')) + "\n") 
